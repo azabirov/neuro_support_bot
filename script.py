78ac48eaa4f470f083cdda0446d60949b847fe6f
Amir <amirzabirov@gmail.com>
2022-02-18 20:29:32 +0300

    Rearrange imports

--- script.py
+++ script.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
-
+import os
+from dotenv import load_dotenv
 import requests
 
 
@@ -32,9 +33,6 @@ def create_intent(project_id, display_name, training_phrases_parts, message_text
 
 
 def main():
-    import os
-    from dotenv import load_dotenv
-
     load_dotenv()
 
     url = input("Введите ссылку на json файл:\n")
